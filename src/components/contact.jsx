581ae705ed1b47b65c104edf46a26611fa62d200
Estarlin R <estarlincito@EstarlincitoM.local>
2022-07-02 15:08:04 -0400

    0.036

--- src/components/contact.jsx
+++ src/components/contact.jsx
@@ -11,19 +11,19 @@ const Contact = () =>{
                 <div className="form__container">
                     <h2 className="form__title">Contáctame</h2>
                     <label className="form__label">Nombre</label>
-                    <input type="text" className="form__input" name="name"/>
+                    <input type="text" className="form__input" name="name" title="Por favor escriba su nombre" required/>
 
                     <label className="form__label">Correo electrónico</label>
-                    <input type="text" className="form__input" name="email"/>
+                    <input type="text" className="form__input" name="email" title="Por favor escriba su correo electrónico" required/>
 
                     <label className="form__label">Número telefónico</label>
-                    <input type="number" className="form__input" name="phoneNumber"/>
+                    <input type="number" className="form__input" name="phoneNumber" title="Por favor escriba su número telefónico" required/>
 
                     <label className="form__label">Asunto</label>
-                    <input type="text" className="form__input" name="requestSubject"/>
+                    <input type="text" className="form__input" name="requestSubject" title="Por favor escriba su asunto" required/>
 
                     <label className="form__label">Mensaje</label>
-                    <textarea className="form__input form__input--message" name="message"></textarea>
+                    <textarea className="form__input form__input--message" name="message" title="Por favor escriba su mensaje" required></textarea>
                     <input type="submit" value="Enviar" className="form__cta"/>
                 </div>
             </form>
